18b82381b3588a68973ce5030b3513aaafe0e8c7
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-22 15:12:45 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,12 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    return {"Hello": "Sarith"}
 
 @app.get("/callname/{name}")
 def read_name(name: str = None):
