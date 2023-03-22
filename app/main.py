c14ba6065d04b4abf91f9ad0971b6077657daacf
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-22 14:49:12 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,5 +16,9 @@ def read_item(item_id: int, q: str = None):
 def read_name(name: str = None):
     return {"hello":name}
 
+@app.get("/callname")
+def write_name(data: dict):
+    name = data.get("name")
+    return {"hello":name}
 
 handler = Mangum(app)
