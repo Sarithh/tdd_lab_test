cfbea91bd80c7c7fd6857f172908fa445a90a1e8
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-22 15:59:05 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -8,10 +8,16 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "Sarith"}
+    assert response.json() == {"Hello": "World"}
     
-def test_write_main():
-    name:"Sarith"
-    response = client.post("/callname", json={"name":name})
+def test_read_call_name():
+    name = "sarith"
+    response = client.get(f"/callname/{name}")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": name}
+
+def test_post_call_name():
+    name = "sarith"
+    response = client.post("/callname", json={'name': name})
     assert response.status_code == 200
     assert response.json() == {"Hello": name}
