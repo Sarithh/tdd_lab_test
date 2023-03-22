9591d59e0282e23a3b8c99b2947cfba2d6be8099
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-22 15:34:31 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,10 +5,15 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Sarith"}
+    return {"Hello": "World"}
 
 @app.get("/callname/{name}")
 def read_name(name: str):
+    return {"hello": name}
+
+@app.post("/callname")
+def write_name(data: dict):
+    name = data.get("name")
     return {"hello": name}
 
 handler = Mangum(app)
