7dedc9d772a86ea8c928212eceffe9b1096da945
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-22 15:16:18 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -14,4 +14,4 @@ def test_write_main():
     name: "Sarith"
     response = client.post("/callname",json={"name":name})
     assert response.status_code == 200
-    assert response.json() == {"Hello": name}
+    assert response.json() == {"hello": name}
