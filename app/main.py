5f026a1699729fff8a078536a19c0d8b63c86b53
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-22 15:57:51 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-from fastapi import Request, FastAPI
-from mangum import Mangum
+from fastapi.testclient import TestClient
 
-app = FastAPI()
+from app.main import app
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+client = TestClient(app)
 
-@app.post("/callname")
-async def post_name(data: dict):
-    name = data.get('name')
-    return {"Hello": name}
 
-@app.get("/callname/{name}")
-def read_name(name: str = None):
-    return {"Hello": name}
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+    
+def test_read_call_name():
+    name = "sarith"
+    response = client.get(f"/callname/{name}")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": name}
 
-handler = Mangum(app)
+def test_post_call_name():
+    name = "sarith"
+    response = client.post("/callname", json={'name': name})
+    assert response.status_code == 200
+    assert response.json() == {"Hello": name}
